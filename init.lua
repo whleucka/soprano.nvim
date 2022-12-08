8a5f89c91ce675be4818ecbcc64b037b52db717e
William Hleucka <william.hleucka@gmail.com>
2022-12-08 14:30:10 -0700

    First

---
+++ init.lua
@@ -0,0 +1,30 @@
+local M = {}
+
+local function is_win()
+  return package.config:sub(1, 1) == '\\'
+end
+
+local function get_path_separator()
+  if is_win() then
+    return '\\'
+  end
+  return '/'
+end
+
+local function script_path()
+  local str = debug.getinfo(2, 'S').source:sub(2)
+  if is_win() then
+    str = str:gsub('/', '\\')
+  end
+  return str:match('(.*' .. get_path_separator() .. ')')
+end
+
+-- Make sure local functions are defined first
+local terminal = require('toggleterm.terminal').Terminal
+local soprano = terminal:new({ cmd = script_path() .. "soprano", hidden = true, direction = "horizontal" })
+
+function M.search()
+    soprano:toggle()
+end
+
+return M
